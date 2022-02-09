24ffc5136ffd3357009d4bf8ed9712449db1fef3
pengbin <pb_2012@126.com>
2022-02-09 16:29:19 +0800

    feat:20220209

---
+++ spring-boot-project/spring-boot-demo/src/main/java/org/springframework/boot/demo/SpringBootDemoApplication.java
@@ -0,0 +1,18 @@
+package org.springframework.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class SpringBootDemoApplication
+{
+    public static void main( String[] args )
+    {
+
+		SpringApplication.run(SpringBootDemoApplication.class,args);
+    }
+}
